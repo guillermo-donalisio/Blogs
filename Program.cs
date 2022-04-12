4e2414d78c804b56b4f63a16ad0b988a1778e004
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-12 12:15:13 -0300

    config

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Blogs.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var hostBuilder = Host.CreateDefaultBuilder() 
+    .ConfigureServices((hostContext, services) =>
+    {
+        //Add you business services
+        var connectionString = hostContext.Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<BlogContext>(x => x.UseSqlServer(connectionString));
+    
+    }).Build();
