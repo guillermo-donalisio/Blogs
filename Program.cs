cb4d2fe7bc56df2f92ade2ae9fad2a329602b225
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-14 18:41:14 -0300

    Project modifications

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
 ﻿using Blogs.Data;
+using Blogs.Repositories;
+using Blogs.Repositories.Implements;
+using Blogs.Service;
+using Blogs.Service.Implements;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-var hostBuilder = Host.CreateDefaultBuilder() 
-    .ConfigureServices((hostContext, services) =>
-    {
-        //Add you business services
-        var connectionString = hostContext.Configuration.GetConnectionString("DefaultConnection");
-        services.AddDbContext<BlogContext>(x => x.UseSqlServer(connectionString));
-    
-    }).Build();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Build DATABASE
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BlogContext>(x => x.UseSqlServer(connectionString));
+
+//Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+//Services
+builder.Services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
